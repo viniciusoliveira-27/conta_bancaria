582437f5b25e13216726ea828dafac3a48fc2f71
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-11 10:21:44 -0300

    :sparkles: Classe Model Conta

--- Menu.ts
+++ Menu.ts
@@ -1,78 +1,88 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Cores";
+import {Conta} from "./src/model/Conta";
 
 let opcao: number
 
-do {
+export function main() {
 
-    menu();
-    opcao = readlinesync.questionInt("Digite a opcao desejada: ")
+    //Cria novas instancias (Objetos) da Classe Conta
+    const c1 = new Conta(1, 123, 1, "Vinicius", 100000);
+    c1.visualizar();
+    console.log(c1.sacar(200000));
+    c1.visualizar();
 
-    if (opcao === 9) {
-        about();
-        process.exit(0)
-    }
+    const c2 = new Conta(1, 123, 2, "Brunildo", 50000);
+    c2.visualizar()
+    c2.depositar(500.00);
+    c2.visualizar()
 
-    switch (opcao) {
-        case 1:
-            console.log(`\nConta criada com Sucesso!\n`)
+    do {
+        console.log(colors.bg.whitebright, colors.fg.blackstrong)
+        console.log("************************************************************")
+        console.log("\n                   ViniBanks                            \n")
+        console.log("************************************************************")
+        console.log("               1 - Criar Conta                              ")
+        console.log("               2 - Listar todas as Contas                   ")
+        console.log("               3 - Buscar Conta por Numero                  ")
+        console.log("               4 - Atualizar Dados da Conta                 ")
+        console.log("               5 - Apagar Conta                             ")
+        console.log("               6 - Sacar                                    ")
+        console.log("               7 - Depositar                                ")
+        console.log("               8 - Transferir valores entre Contas          ")
+        console.log("               9 - Sair                                     ")
+        console.log("\n************************************************************\n", colors.reset)
+        opcao = readlinesync.questionInt("Digite a opcao desejada: ")
 
-            break;
-        case 2:
-            console.log(`\nTodas as contas: \n`)
+        if (opcao === 9) {
+            about();
+            process.exit(0)
+        }
 
-            break;
-        case 3:
-            console.log(`\nFazendo Busca pelo Numero da conta...\n`)
+        switch (opcao) {
+            case 1:
+                console.log(`\nConta criada com Sucesso!\n`)
 
-            break;
-        case 4:
-            console.log(`\nConta Atualizada\n`)
-            break;
+                break;
+            case 2:
+                console.log(`\nTodas as contas: \n`)
 
-        case 5:
-            console.log(`\nApagar conta!\n`)
-            break;
+                break;
+            case 3:
+                console.log(`\nFazendo Busca pelo Numero da conta...\n`)
 
-        case 6:
-            console.log(`\nSaque!\n`)
-            break;
+                break;
+            case 4:
+                console.log(`\nConta Atualizada\n`)
+                break;
 
-        case 7:
-            console.log(`\nDeposito\n`)
-            break;
+            case 5:
+                console.log(`\nApagar conta!\n`)
+                break;
 
-        case 8:
-            console.log(`\nPIX!\n`)
-            break;
+            case 6:
+                console.log(`\nSaque!\n`)
+                break;
 
-        case 9:
-            break;
+            case 7:
+                console.log(`\nDeposito\n`)
+                break;
 
-        default:
-            console.log("Opcao invalida!")
-    }
+            case 8:
+                console.log(`\nPIX!\n`)
+                break;
 
-} while (true)
+            case 9:
+                break;
 
+            default:
+                console.log("Opcao invalida!")
+        }
 
-function menu(): void {
-    console.log(colors.bg.whitebright, colors.fg.blackstrong)
-    console.log("************************************************************")
-    console.log("\n                   ViniBanks                            \n")
-    console.log("************************************************************")
-    console.log("               1 - Criar Conta                              ")
-    console.log("               2 - Listar todas as Contas                   ")
-    console.log("               3 - Buscar Conta por Numero                  ")
-    console.log("               4 - Atualizar Dados da Conta                 ")
-    console.log("               5 - Apagar Conta                             ")
-    console.log("               6 - Sacar                                    ")
-    console.log("               7 - Depositar                                ")
-    console.log("               8 - Transferir valores entre Contas          ")
-    console.log("               9 - Sair                                     ")
-    console.log("\n************************************************************\n", colors.reset)
+    } while (true)
 
-}
+} 
 function about() {
     console.log("\nCriado por Vinicius\n")
 }
+main()
---
+++ src/model/Conta.ts
@@ -0,0 +1,103 @@
+export class Conta{
+
+    //definição do modelo de dados (esses são os atributos da conta)
+    private _numero: number;
+    private _agencia: number;
+    private _tipo: number;
+    private _titular: string;
+    private _saldo: number;
+
+    // Gerar o metodo construtor
+	constructor(numero: number, agencia: number, tipo: number, titular: string, saldo: number) {
+		this._numero = numero;
+		this._agencia = agencia;
+		this._tipo = tipo;
+		this._titular = titular;
+		this._saldo = saldo;
+	}
+
+    //Gerar os metodos Getter's e Setter's
+
+	public get numero(): number {
+		return this._numero;
+	}
+
+	public get agencia(): number {
+		return this._agencia;
+	}
+
+	public get tipo(): number {
+		return this._tipo;
+	}
+
+	public get titular(): string {
+		return this._titular;
+	}
+
+	public get saldo(): number {
+		return this._saldo;
+	}
+
+
+    //Setters
+	public set numero(value: number) {
+		this._numero = value;
+	}
+
+	public set agencia(value: number) {
+		this._agencia = value;
+	}
+
+	public set tipo(value: number) {
+		this._tipo = value;
+	}
+
+	public set titular(value: string) {
+		this._titular = value;
+	}
+
+	public set saldo(value: number) {
+		this._saldo = value;
+	}
+
+    //Metodo Sacar
+    public sacar(valor: number): boolean {
+        if(valor > this._saldo){
+            console.log("Saldo Insuficiente!")
+            return false;
+        }
+
+        this._saldo -= valor
+        return true;
+    }
+
+    //Metodo depositar
+    public depositar(valor: number) {
+        this._saldo += valor
+    }
+
+
+    //metodo para visualizar os dados do objeto
+    public visualizar() {
+        let tipo: string;
+
+        switch(this._tipo) {
+            case 1:
+                tipo = "Conta corrente"
+                break;
+            case 2:
+                tipo = "Conta Poupança"
+                break;
+            default:
+                tipo = "Tipo de conta Invalida"
+        }
+        console.log("************************************")
+        console.log("\n         Dados da Conta\n")
+        console.log("************************************\n")
+        console.log(`Numero da Conta: ${this._numero}`)
+        console.log(`Numero da Agencia: ${this._agencia}`)
+        console.log(`Tipo da Conta: ${tipo}`)
+        console.log(`Nome do Titular da Conta: ${this._titular}`)
+        console.log(`Saldo da Conta: ${this._saldo}\n`)
+    }
+}
